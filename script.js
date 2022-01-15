dbb83d5ef2a9855e64ad13fde7911504ce45a5fe
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-15 22:16:37 +0800

    Added codes about Handling a Keypress Event

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+//Working with Classes
 const openModal = function () {
   //Removes class name 'hidden' to make it appear
   modal.classList.remove('hidden');
@@ -24,7 +25,15 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 btnCloseModal.addEventListener('click', closeModal); //No () in the function so it would not be called immediately
-
 overlay.addEventListener('click', closeModal);
 
-//Working with Classes
+//Handling a keypress event
+
+//Adding a parameter in the function and logging it generates details on which key was pressed.
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
