b1bd400c76cc986c83101eeed52d26c1bf2b8687
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-15 21:57:24 +0800

    Added codes about Working and Classes

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  //Removes class name 'hidden' to make it appear
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  //Adds back the 'hidden' class to make it disappear
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal); //No () in the function so it would not be called immediately
+
+overlay.addEventListener('click', closeModal);
+
+//Working with Classes
